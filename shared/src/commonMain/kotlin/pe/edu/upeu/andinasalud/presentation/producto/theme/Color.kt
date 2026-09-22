4f034685059391669df3364a55a37af8aaf9790b
DIEGO <luckyilose23@gmail.com>
2026-09-22 15:26:11 -0500

    style(tema): agrega AndinaSaludTheme con paleta propia y soporte claro/oscuro completo

---
+++ shared/src/commonMain/kotlin/pe/edu/upeu/andinasalud/presentation/producto/theme/Color.kt
@@ -0,0 +1,20 @@
+package pe.edu.upeu.andinasalud.presentation.theme
+
+import androidx.compose.ui.graphics.Color
+
+val AzulPrimario = Color(0xFF0B6E99)
+val AzulPrimarioClaro = Color(0xFFD3E9F5)
+val CianSecundario = Color(0xFF4FB3BF)
+val VerdeExito = Color(0xFF2E7D5B)
+val FondoClaro = Color(0xFFF6FAFB)
+val SuperficieClara = Color(0xFFFFFFFF)
+val TextoClaro = Color(0xFF17242B)
+val ErrorClaro = Color(0xFFBA1A1A)
+
+val AzulPrimarioOscuro = Color(0xFF7FCBEF)
+val AzulPrimarioOscuroContainer = Color(0xFF004D6B)
+val CianSecundarioOscuro = Color(0xFF8FD9E1)
+val FondoOscuro = Color(0xFF10181C)
+val SuperficieOscura = Color(0xFF1A2328)
+val TextoOscuro = Color(0xFFDCE4E8)
+val ErrorOscuro = Color(0xFFFFB4AB)
